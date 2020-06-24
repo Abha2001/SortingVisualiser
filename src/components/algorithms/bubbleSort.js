bf241e770bd02d7f009682497700d22a77601ced
Abha Jha <jhaabha08@gmail.com>
2020-06-24 20:33:55 +0530

    added animations

--- src/components/algorithms/bubbleSort.js
+++ src/components/algorithms/bubbleSort.js
@@ -1,26 +1,27 @@
-import React from 'react';
-
 var BubbleSortAlgo =(props)=>{
+    var animations=[];
+    var index=0;
     // console.log(props.length);
-    for (var i=0;i<props.length-1;i++)
+    for (var i=0;i<props.length;i++)
     {
         for(var j=0;j<props.length-i-1;j++)
         {
+            animations[index]=[j,j+1];
             if (props[j]>props[j+1])
             {
+                animations[index].push(1);
                 var t=props[j];
                 props[j]=props[j+1];
                 props[j+1]=t;
             }
+            else
+            {
+                animations[index].push(0);
+            }
+            index++;
         }
     }
-    console.log(props)
-    return(
-        <div>
-
-        </div>
-    )
-
+    return animations;
 }
 
 export default BubbleSortAlgo;
--- src/components/animations/bubbleAnimations.js
+++ src/components/animations/bubbleAnimations.js
@@ -11,11 +11,53 @@ class BubbleSort extends Component
         this.generateRandom();
     }
     sortArray=()=>{
-        BubbleSortAlgo(this.state.array)
+        var animationsArray=BubbleSortAlgo(this.state.array);
+        console.log("hi");
+        this.startAnimations(animationsArray);
     }
+    startAnimations(animationsArray)
+    {
+        animationsArray.forEach((comparision,i)=>{
+            var firstElem=comparision[0];
+            var secondElem= comparision[1];
+            var swap = comparision[2];
+            // console.log(firstElem,secondElem)
+            setTimeout(()=>{
+                console.log(firstElem,secondElem)
+                this.changeColourCompare(firstElem,secondElem);
+            },(i+0.25)*50)
+            setTimeout(()=>{this.changeBackColour(firstElem,secondElem);},(i+0.5)*50)
+            if(swap)
+            {
+                setTimeout(()=>{
+                    this.changeColourSwap(firstElem,secondElem);
+                },(i+1)*50)
+            }
+        })
+    }
+
+    changeColourCompare=(i,j)=>{
+        // console.log("changing colour")
+        document.getElementsByClassName('bar')[i].style.backgroundColor='red';
+        document.getElementsByClassName('bar')[j].style.backgroundColor='red';
+    }
+    changeBackColour=(i,j)=>{
+        document.getElementsByClassName('bar')[i].style.backgroundColor='blueviolet';
+        document.getElementsByClassName('bar')[j].style.backgroundColor='blueviolet';
+    }
+    changeColourSwap=(i,j)=>{
+            var ElemOneHt=document.getElementsByClassName('bar')[i].style.height;
+            var ElemTwoHt=document.getElementsByClassName('bar')[j].style.height;
+            var t=ElemOneHt;
+            // console.log(ElemOneHt,ElemTwoHt,t)
+            document.getElementsByClassName('bar')[i].style.height=ElemTwoHt;
+            document.getElementsByClassName('bar')[j].style.height=t;
+        
+    }
+
     generateRandom=()=>{
         var arr=[]
-        for (var i=1;i<=100;i++)
+        for (var i=1;i<=50;i++)
         {
             var num=Math.floor((Math.random() * 100) + 1);
             arr.push(num);
